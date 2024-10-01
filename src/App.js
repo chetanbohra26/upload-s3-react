1e501e5e0971d479dc816c156d0e5f750884ff50
Chetan Bohra <chetan.bohra@squareboat.com>
2024-10-01 23:55:12 +0530

    Added upload calls

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef } from 'react';
+import axios from 'axios';
 
 function App() {
+  const filePickerRef = useRef();
+
+  const testUpload = async () => {
+    const file = filePickerRef?.current?.files?.[0];
+    const fileName = file?.name?.split('.')[0];
+    const ext = file?.name?.split('.')[1];
+    const mime = file?.type;
+
+    const payload = {
+      n: 1,
+      path: process.env.REACT_APP_BUCKET_PATH,
+      extensions: [ext],
+      fileNames: [fileName],
+    };
+
+    const URL = process.env.REACT_APP_UPLOAD_URL;
+
+    axios.post(URL, payload)
+      .then((response) => {
+        const url = response.data.data[0].url;
+
+        axios.put(url, file, {
+          headers: {
+            'Content-Type': mime,
+          },
+          onUploadProgress: (event) => {
+            const { loaded, total } = event;
+            let precentage = Math.floor((loaded * 100) / total);
+            console.log(precentage);
+          },
+        }).then(data => console.log(data))
+          .catch(err => console.log(err));
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: 10 }}>
+      <input type="file" placeholder='Pick file' ref={filePickerRef} onChange={() => testUpload()} />
     </div>
   );
 }
